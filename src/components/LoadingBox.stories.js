e0fd24b6dd29c9fa0228a8b730524152adfe2145
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-06-25 15:40:53 +0900

    create LoadingBox.js create LoadingBox.js and LoadingBox.stories.js

---
+++ src/components/LoadingBox.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import StoryRouter from "storybook-react-router";
+
+import LoadingBox from "./LoadingBox";
+
+storiesOf("LoadingBox", module)
+  .addDecorator(StoryRouter())
+  .add("default", () => <LoadingBox />);
