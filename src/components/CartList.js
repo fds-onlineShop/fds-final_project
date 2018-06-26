2035119291ed48661c8ac3572bc0e63f0b917d7c
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-06-26 16:01:19 +0900

    CartList 주석추가

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import CartListItem from './CartListItem';
 
 export default function CartList({ carts = [], onRemoveCartItem = () => {} }) {
+  // Cart 총합계 금액 계산식
   let cartTotalPrice = 0;
   for (let i = 0; i < carts.length; i++) {
     cartTotalPrice += parseFloat(carts[i].price.replace(/\$/, ''));
   }
   return carts.length > 0 ? (
+    // Cart가 비어있지 않은 경우
     <div className="cart-item">
       <ul className="cart-item__list">
         {carts.map(cart => (
@@ -23,6 +25,7 @@ export default function CartList({ carts = [], onRemoveCartItem = () => {} }) {
       </Link>
     </div>
   ) : (
+    // Cart가 비어있는 경우
     <div className="cart-noitem">
       <p>The Cart is empty.</p>
       <Link to="/" className="button">
