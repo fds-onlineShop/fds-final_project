0d7234badd7163461784b5a0b9a88977edefc56c
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-07-04 14:29:53 +0900

    Order페이지 스타일링

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -23,63 +23,69 @@ export default function OrderForm({
       }}
       className="orderForm"
     >
-      <section className="orderForm__address">
-        <h3>Shipping Address</h3>
-        <div className="field">
-          <div className="control">
-            <label htmlFor="address" className="label">
-              Address
-            </label>
-            <input
-              type="text"
-              required
-              className="input"
-              id="address"
-              value={orderAddress}
-              onChange={e => onAddressChange(e.target.value)}
-            />
+      <section className="orderForm__container">
+        <article className="orderForm__address">
+          <h3 className="title is-5">Shipping Address</h3>
+          <div className="field orderForm__address__form">
+            <div className="control">
+              <label htmlFor="address" className="label">
+                Address
+              </label>
+              <input
+                type="text"
+                required
+                className="input"
+                id="address"
+                value={orderAddress}
+                onChange={e => onAddressChange(e.target.value)}
+              />
+            </div>
+            <div className="control">
+              <label htmlFor="phone" className="label">
+                Phone
+              </label>
+              <input
+                type="text"
+                required
+                className="input"
+                id="phone"
+                value={orderPhone}
+                onChange={e => onPhoneChange(e.target.value)}
+              />
+            </div>
+            <div className="control">
+              <label htmlFor="email" className="label">
+                Email
+              </label>
+              <input
+                type="text"
+                required
+                className="input"
+                id="email"
+                value={orderEmail}
+                onChange={e => onEmailChange(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="control">
-            <label htmlFor="phone" className="label">
-              Phone
-            </label>
-            <input
-              type="text"
-              required
-              className="input"
-              id="phone"
-              value={orderPhone}
-              onChange={e => onPhoneChange(e.target.value)}
-            />
+        </article>
+        <article className="orderForm__order">
+          <h3 className="title is-5">Shopping Bag</h3>
+          <ul className="orderForm__order__list">
+            {orderItems.map((orderItem, index) => (
+              <OrderFormItem key={index} {...orderItem} />
+            ))}
+          </ul>
+          <div className="orderForm__order__total">
+            <span className="orderForm__order__total__heading">Total</span>
+            <span className="orderForm__order__total__price">
+              ${orderTotalPrice}
+            </span>
           </div>
-          <div className="control">
-            <label htmlFor="email" className="label">
-              Email
-            </label>
-            <input
-              type="text"
-              required
-              className="input"
-              id="email"
-              value={orderEmail}
-              onChange={e => onEmailChange(e.target.value)}
-            />
-          </div>
-        </div>
+        </article>
       </section>
-      <section className="orderForm__order">
-        <h3>Shopping Bag</h3>
-        <ul className="orderForm__list">
-          {orderItems.map((orderItem, index) => (
-            <OrderFormItem key={index} {...orderItem} />
-          ))}
-        </ul>
-        <div className="orderForm__total">
-          <span className="orderForm__total__heading">Total</span>
-          <span className="orderForm__total__price">${orderTotalPrice}</span>
-        </div>
+      <section className="orderForm__order__payment">
+        <button className="button is-black">Payment</button>
       </section>
-      <button className="button">Payment</button>
     </form>
   );
 }
