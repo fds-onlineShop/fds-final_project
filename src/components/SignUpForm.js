521951e35d2506422df584f37f9240f14f39eea4
Kim Sanghyeok <skyee23@gmail.com>
2018-07-04 23:02:01 +0900

    회원가입 폼 디자인 수정

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -36,68 +36,85 @@ export default class SignUpForm extends React.Component {
 
     return (
       <React.Fragment>
-        <form onSubmit={this.handleSubmit}>
-          <div className="field">
-            <label className="label">Username</label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                value={username}
-                onChange={e => onUsernameChange(e.target.value)}
-              />
+        <section className="hero is-success is-fullheight login-form_layout">
+          <div className="hero-body login-form">
+            <div className="container has-text-centered">
+              <div className="column is-4 is-offset-4">
+                <h3 className="title has-text-grey">Sign Up</h3>
+                <div className="box">
+                  <figure className="avatar">
+                    <img
+                      alt="loginImg"
+                      src="https://www.shareicon.net/data/128x128/2016/04/07/746062_multimedia_512x512.png"
+                    />
+                  </figure>
+                  <form onSubmit={this.handleSubmit}>
+                    <div className="field">
+                      <label className="label">Username</label>
+                      <div className="control">
+                        <input
+                          className="input "
+                          type="text"
+                          autoFocus=""
+                          value={username}
+                          onChange={e => onUsernameChange(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">Password</label>
+                      <div className="control">
+                        <input
+                          className="input "
+                          type="password"
+                          value={password}
+                          onChange={e => onPasswordChange(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">Address</label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          type="text"
+                          value={address}
+                          onChange={e => onAddressChange(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">Phone</label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          type="tel"
+                          value={phone}
+                          onChange={e => onPhoneChange(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">E-mail</label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          type="email"
+                          value={email}
+                          onChange={e => onEmailChange(e.target.value)}
+                        />
+                      </div>
+                    </div>
+
+                    <button className="button is-block is-info is-large is-fullwidth">
+                      Sign Up
+                    </button>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="field">
-            <label className="label">Password</label>
-            <div className="control">
-              <input
-                className="input"
-                type="password"
-                value={password}
-                onChange={e => onPasswordChange(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Address</label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                value={address}
-                onChange={e => onAddressChange(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Phone</label>
-            <div className="control">
-              <input
-                className="input"
-                type="tel"
-                value={phone}
-                onChange={e => onPhoneChange(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">E-mail</label>
-            <div className="control">
-              <input
-                className="input"
-                type="email"
-                value={email}
-                onChange={e => onEmailChange(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="field is-grouped">
-            <div className="control">
-              <button className="button is-link">Sign Up</button>
-            </div>
-          </div>
-        </form>
+        </section>
       </React.Fragment>
     );
   }
