3063efe7fdac78734643a7a3dc1e7084664db89b
Kim Sanghyeok <skyee23@gmail.com>
2018-07-06 12:00:26 +0900

    merge

--- src/components/TopItem.js
+++ src/components/TopItem.js
@@ -9,50 +9,25 @@ export default function TopItem({
   onSubmit = () => {},
   handleOver = () => {},
   handleOut = () => {},
+  ClickEvent = () => {},
+  RemoveEvent = () => {},
 }) {
   return (
-    <div>
-      <form>
-        <img
-          onMouseOver={e => {
-            e.preventDefault();
-            handleOver(id);
-          }}
-          onMouseOut={e => {
-            e.preventDefault();
-            handleOut(id);
-          }}
-          src={imgurl}
-          alt={title}
-          id="img"
-          className="top-item__list__img"
-        />
-        <div id="title" className="top-item__list__title">
-          {title}
+    <div className="top-item__layout">
+      <div className="control">
+        <div className="content">
+          <a onClick={e => ClickEvent(id)}>
+            <img
+              src={imgurl}
+              alt={title}
+              id="img"
+              className="card-image"
+              onClick={e => RemoveEvent()}
+            />
+          </a>
+          <div className="title is-4">{title}</div>
         </div>
-        <div id="body" className="top-item__list__body">
-          {body}
-        </div>
-        <div id="price" className="top-item__list__price">
-          {price}
-        </div>
-        <button onClick={e => onSubmit(id)}>add to my cart</button>
-      </form>
+      </div>
     </div>
-    // <div className="top-item__layout">
-    //   <div className="control">
-    //     <form className="content">
-    //       <a>
-    //         <img src={imgurl} alt={title} id="img" className="card-image" />
-    //       </a>
-    //       <div className="title is-4">{title}</div>
-    //       <div className="subtitle is-6">{body}</div>
-    //       <div className="subtitle is-6">{price}</div>
-    //       <button className="button is-light" onClick={e => onSubmit(id)}>
-    //         cart
-    //       </button>
-    //     </form>
-    //   </div>
-    // </div>
   );
 }
--- src/containers/TopPreContainer.js
+++ src/containers/TopPreContainer.js
@@ -2,33 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { TopListConsumer } from '../contexts/TopListContext';
 import LoadingBox from '../components/LoadingBox';
-import TopList from '../components/TopList';
+import TopPreList from '../components/TopPreList';
 
 export default class TopListContainer extends React.Component {
   render() {
     return (
       <TopListConsumer>
-        {({
-          loading,
-          tops,
-          submit,
-          handleOver,
-          handleOut,
-          ClickEvent,
-          infor,
-          RemoveEvent,
-        }) =>
+        {({ loading, submit, infor, RemoveEvent }) =>
           loading ? (
             <LoadingBox />
           ) : (
-            <TopList
-              tops={tops}
+            <TopPreList
+              infor={infor}
               onSubmit={submit}
               brandAs={Link}
-              handleOver={handleOver}
-              handleOut={handleOut}
-              ClickEvent={ClickEvent}
-              infor={infor}
               RemoveEvent={RemoveEvent}
             />
           )
